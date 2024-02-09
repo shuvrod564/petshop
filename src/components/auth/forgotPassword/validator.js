3275e44cc948baf3fe54f77083c056355f2f7a50
shuvrod564 <shuvrod564@gmail.com>
2024-02-09 10:11:08 +0600

    error auth

--- src/components/auth/forgotPassword/validator.js
+++
@@ -1,7 +0,0 @@
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as Yup from "yup";
-
-const validationSchema = Yup.object().shape({
-    email: Yup.string().email('Must be a valid email').max(255).required('Please enter your email'),
-  });
-  export const formOptions = { resolver: yupResolver(validationSchema) };
