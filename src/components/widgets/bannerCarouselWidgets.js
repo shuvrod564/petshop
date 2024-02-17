0f009869d5f830cf4acd1e9d754281363fe1bdee
shuvrod564 <shuvrod564@gmail.com>
2024-02-17 17:02:05 +0600

    banner

--- src/components/widgets/bannerCarouselWidgets.js
+++ src/components/widgets/bannerCarouselWidgets.js
@@ -40,29 +40,13 @@ const BannerCarouselWidgets = () => {
   return (
       <>
       <div className="container py-6"> 
-      <div className="bg-[url('/images/global/gradient-abstract-background.png')] bg-no-repeat bg-cover bg-center rounded-lg px-6 py-7 md:py-8 lg:px-10">
-
-        <Slider {...settings} className="hero_carousel">
-          {
-            data.map((item, index)=> {
-              return( 
-                <div key={index} className="!flex flex-row flex-wrap items-center">
-                  <div className="lg:w-[350px] md:w-[280px] w-full">
-                    <Image src={item.thumbnail} width={500} height={500} alt="thumbnail" />
-                  </div>
-                  <div className="col lg:pl-14 md:pl-6 py-5 md:py-0 text-center md:text-left">
-                    <p className={`${poppins.className} text-xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold text-white`}>{item.title}</p>
-                    <p className="text-lg text-[#33E120]"><i>Its Taste so Good</i></p>
-                    <p className="text-lg md:text-2xl lg:text-3xl font-light text-white my-4 lg:my-5"><i>Starting From <b className="font-bold">₹199</b> Only</i></p>
-                    <Link href={"#"} className="text-base md:text-lg px-5 py-3 bg-primary text-white rounded-lg hover:bg-primary-light hover:text-primary">Buy Now</Link>
-                  </div>
-                </div>
-              )
-            })
-          }
-           
-        </Slider>
-      </div>
+        <div className="shoadow border border-[#ddd]">
+          <Image 
+            src={"/images/global/hero-banner.jpg"} 
+            width={1340} height={550} alt="thumbnail" 
+            className=""
+          />
+        </div>
       </div>
     </>
   )
